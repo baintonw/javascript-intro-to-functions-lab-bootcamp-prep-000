7c5c0bd42ad83196e733c1df616d739f3bbe4d32
William Bainton <baintonw@gmail.com>
2019-03-31 01:15:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,17 @@ function whisper(string) {
 function logShout(string) {
   console.log(string.toUpperCase)
 }
+
+var uppercase = "HELLO!"
+ 
+uppercase.toUpperCase() === uppercase // true
+ 
+var lowercase = 'hello!'
+ 
+lowercase.toLowerCase() === lowercase // true
+ 
+var mixedCase = 'Hi there!'
+ 
+mixedCase.toLowerCase() === mixedCase // false
+ 
+mixedCase.toUpperCase() === mixedCase // false
