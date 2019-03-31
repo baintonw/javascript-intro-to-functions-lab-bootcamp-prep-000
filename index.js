70cdb7b30b4bedf703754ca844623958f1cf7dc3
William Bainton <baintonw@gmail.com>
2019-03-31 02:01:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,10 +19,10 @@ function sayHiToGrandma(string) {
   return "I can't hear you!"
 }
   if (string.toUpperCase === string) {
-    return 'YES INDEED!'
+    return "YES INDEED!"
 }
   if (string === "I love you, Grandma.") {
-    return 'I love you, too.'
+    return "I love you, too."
 }
 }
 
